dda8d44f750a36278350dd5ead560dd3f00eac6a
Jude Lopez <70026775+ExoticBubble15@users.noreply.github.com>
2024-05-30 03:03:43 -0700

    creates node cells without edges

--- circuits.py
+++ circuits.py
@@ -1,21 +1,64 @@
 import cv2
-from PIL import ImageGrab, Image
+from PIL import ImageGrab
 import numpy as np
 import pytesseract
-import time
 
-# xlength, ylength = 1920, 1080
-# xoffset, yoffsettop, yoffsetbot = 315, 15, 325
+#for correcting ocr readings
+VALID = "1234567890qwertyuiopasdfghjklzxcvbnm"
+def correct(str):
+    str = str.lower()
+    ret = ""
+    for i in range(len(str)):
+        if VALID.find(str[i]) != -1:
+            ret += str[i]
+    return ret
+
+#node class for representing cells
+class Node:
+    def __init__(self, text):
+        self.text = text
+        self.top, self.bot, self.left, self.right = None, None, None, None
+
+    def setTop(self, top):
+            self.top = top
+
+    def setBot(self, bot):
+            self.bot = bot
+
+    def setLeft(self, left):
+            self.left = left
+
+    def setRight(self, right):
+            self.right = right
+
+wordGraph = [["","","",""], ["","","",""], ["","","",""], ["","","",""]]    #creates virtual game board: '~' denotes user inputted space, [y][x] for accessing
+def display(prefix, arr):
+    print(f'\n{prefix}')
+    for i in range(len(arr)):
+        print(arr[i])
+
 BOXCOORDS = [1920/2-315, 1080/2-15, 1920/2+315, 1080/2+325]
+YADJ = 20   #adjusting cell view area so ocr works
 XSEGMENT, YSEGMENT = (BOXCOORDS[2]-BOXCOORDS[0])/4, (BOXCOORDS[3]-BOXCOORDS[1])/4
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-for ycount in range(4):
-    for xcount in range(4):
-    # img = ImageGrab.grab(bbox=(xlength/2-xoffset, ylength/2-yoffsettop, xlength/2+xoffset, ylength/2+yoffsetbot))
-    # img = ImageGrab.grab(bbox=(BOXCOORDS[0], BOXCOORDS[1], BOXCOORDS[2], BOXCOORDS[3]))
-        img = ImageGrab.grab(bbox=(BOXCOORDS[0]+xcount*XSEGMENT, BOXCOORDS[1]+ycount*YSEGMENT, BOXCOORDS[0]+(xcount+1)*XSEGMENT, BOXCOORDS[1]+(ycount+1)*YSEGMENT))
+#first iteration to create cells
+for yCount in range(4):
+    for xCount in range(4):
+        img = ImageGrab.grab(bbox=(BOXCOORDS[0]+xCount*XSEGMENT, BOXCOORDS[1]+yCount*YSEGMENT+YADJ, BOXCOORDS[0]+(xCount+1)*XSEGMENT, BOXCOORDS[1]+(yCount+1)*YSEGMENT-YADJ))
         img = np.array(img)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        cv2.imshow("", img)
-        cv2.waitKey(0)
+        word = correct(pytesseract.image_to_string(img))
+        if len(np.where(img==[0])[0]) != 0: #detecting if area has any black and thus is nonempty
+            if word == "":
+                word = "~"
+            wordGraph[yCount][xCount] = word#Node(word)
+        print(f'{xCount}, {yCount}: {word}')
+        # cv2.imshow("", img)
+        # cv2.waitKey(250)
+
+display("word graph", wordGraph)
+# print(wordGraph[0][1].text)
+
+#second iteration to create edges
+for yCount in range(4):
+    for xCount in range(4):
